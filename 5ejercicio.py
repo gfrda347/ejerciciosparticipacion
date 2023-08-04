1cc15a9b90ab13d2c0ad140d5cd505a21b70d21b
Santiago <davidsantiagorodriguezruiz9@gmail.com>
2023-08-04 08:17:45 -0500

    nuevo ejercicio

--- 5ejercicio.py
+++ 5ejercicio.py
@@ -1,1 +1,12 @@
 #Crear una función que convierta grados Fahrenheit a grados Celsius.
+def fahrenheit_a_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+
+def main():
+    grados_fahrenheit = 68
+    grados_celsius = fahrenheit_a_celsius(grados_fahrenheit)
+    print(f"{grados_fahrenheit} grados Fahrenheit equivalen a {grados_celsius:.2f} grados Celsius.")
+
+if __name__ == "__main__":
+    main()
